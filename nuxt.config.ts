80b1720a1d1ef23f697733471cf6f06362a8b260
chaksack <chakdahah@gmail.com>
2026-08-12 12:25:23 +0000

    initialisation

---
+++ nuxt.config.ts
@@ -0,0 +1,109 @@
+import tailwindcss from '@tailwindcss/vite'
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+
+  css: ['~/assets/css/tailwind.css'],
+  vite: {
+    plugins: [tailwindcss()],
+    optimizeDeps: {
+      include: [
+        '@internationalized/date',
+        '@number-flow/vue',
+        '@tanstack/vue-table',
+        '@unovis/ts',
+        '@unovis/vue',
+        '@vee-validate/zod',
+        '@vueuse/core',
+        '@vueuse/math',
+        'class-variance-authority',
+        'clsx',
+        'date-fns',
+        'embla-carousel',
+        'embla-carousel-vue',
+        'lucide-vue-next',
+        'nanoid',
+        'pinia',
+        'reka-ui',
+        'reka-ui/date',
+        'tailwind-merge',
+        'vaul-vue',
+        'vee-validate',
+        'vue-sonner',
+        'vuedraggable',
+        'zod',
+      ],
+    },
+  },
+
+  components: [
+    {
+      path: '~/components',
+      extensions: ['.vue'],
+    },
+  ],
+
+  modules: [
+    'shadcn-nuxt',
+    '@vueuse/nuxt',
+    '@nuxt/eslint',
+    '@nuxt/icon',
+    '@pinia/nuxt',
+    '@nuxtjs/color-mode',
+    '@nuxt/fonts',
+  ],
+
+  shadcn: {
+    /**
+     * Prefix for all the imported component
+     */
+    prefix: '',
+    /**
+     * Directory that the component lives in.
+     * @default "~/components/ui"
+     */
+    componentDir: '~/components/ui',
+  },
+
+  colorMode: {
+    classSuffix: '',
+  },
+
+  eslint: {
+    config: {
+      standalone: false,
+    },
+  },
+
+  fonts: {
+    defaults: {
+      weights: [300, 400, 500, 600, 700, 800],
+    },
+    providers: {
+      fontshare: false,
+    },
+  },
+
+  routeRules: {
+    '/components': { redirect: '/components/accordion' },
+    '/settings': { redirect: '/settings/profile' },
+  },
+
+  imports: {
+    dirs: ['./lib'],
+  },
+
+  compatibilityDate: '2026-03-13',
+
+  nitro: {
+    prerender: {
+      ignore: [
+        '/examples/forms',
+        '/terms',
+        '/privacy',
+        '/components/pagination',
+        '/docs',
+      ],
+    },
+  },
+})
